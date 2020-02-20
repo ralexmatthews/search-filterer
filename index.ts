faecba0679c20004792953fee12b70e2fdbbb510
Alex Matthews <amatthews@setup85s-mbp.dmp.corp>
2020-02-20 08:41:49 -0600

    Tweaks algorithms

--- index.ts
+++ index.ts
@@ -10,102 +10,79 @@ import {
   zip
 } from "ramda";
 
-const distance: (a: string, b: string) => number = curry(
-  memoizeWith(
-    (string1: string, string2: string) =>
-      string1.length > string2.length
-        ? `${string1}${string2}`
-        : `${string2}${string1}`,
-    (a: string, b: string) => {
-      if (!a.length || !b.length) {
-        return 0;
-      }
-      const string1 = a.trim();
-      const string2 = b.trim();
+const distance: (a: string, b: string, vague?: boolean) => number = memoizeWith(
+  (string1: string, string2: string, vague?: boolean) =>
+    string1.length > string2.length
+      ? `${string1}${string2}${vague}`
+      : `${string2}${string1}${vague}`,
+  (string1: string, string2: string, vague?: boolean) => {
+    if (!string1.length || !string2.length) {
+      return 0;
+    }
+    const a = string1.trim();
+    const b = string2.trim();
 
-      if (string1 === string2) {
-        return -2;
-      }
+    if (a === b) {
+      return -2;
+    }
 
-      const split1 = string1.split(" ");
-      const split2 = string2.split(" ");
+    if (vague) {
+      const split1 = a.split(" ");
+      const split2 = b.split(" ");
       if (split1.length > 1 && split2.length === split1.length) {
         return mean(zip(split1, split2).map(v => distance(...v)));
       }
-
-      if (string1.length > string2.length) {
-        if (string1.substr(0, string2.length) === string2) {
-          return -1;
-        }
-        if (string1.includes(string2)) {
-          return 0;
-        }
-      } else if (string2.length > string1.length) {
-        if (string2.substr(0, string1.length) === string1) {
-          return -1;
-        }
-        if (string2.includes(string1)) {
-          return 0;
-        }
-      }
-
-      const d = [];
-
-      for (let i = 0; i <= string1.length; i++) {
-        d.push([i]);
-      }
-
-      for (let j = 0; j <= string2.length; j++) {
-        d[0][j] = j;
-      }
-
-      for (let i = 1; i <= string1.length; i++) {
-        for (let j = 1; j <= string2.length; j++) {
-          const cost = string1[i] === string2[j] ? 0 : 1;
-
-          d[i][j] = min(
-            min(d[i - 1][j] + 1, d[i][j - 1] + 1),
-            d[i - 1][j - 1] + cost
-          );
-
-          if (
-            i > 1 &&
-            j > 1 &&
-            string1[i] === string2[j - 1] &&
-            string1[i - 1] === string2[j]
-          ) {
-            d[i][j] = min(d[i][j], d[i - 2][j - 2] + 1);
-          }
-        }
-      }
-
-      return d[string1.length][string2.length];
     }
-  )
+
+    if (a.length > b.length) {
+      if (a.substr(0, b.length) === b) {
+        return -1;
+      }
+      if (a.includes(b)) {
+        return 0;
+      }
+    } else if (b.length > a.length) {
+      if (b.substr(0, a.length) === a) {
+        return -1;
+      }
+      if (b.includes(a)) {
+        return 0;
+      }
+    }
+
+    const d = [];
+
+    for (let i = 0; i <= a.length; i++) {
+      d.push([i]);
+    }
+
+    for (let j = 0; j <= b.length; j++) {
+      d[0][j] = j;
+    }
+
+    for (let i = 1; i <= a.length; i++) {
+      for (let j = 1; j <= b.length; j++) {
+        const cost = a[i - 1] === b[j - 1] ? 0 : 1;
+
+        d[i][j] = min(
+          min(d[i - 1][j] + 1, d[i][j - 1] + 1),
+          d[i - 1][j - 1] + cost
+        );
+
+        if (i > 1 && j > 1 && a[i - 1] === b[j - 2] && a[i - 2] === b[j - 1]) {
+          d[i][j] = min(d[i][j], d[i - 2][j - 2] + 1);
+        }
+      }
+    }
+
+    return d[a.length][b.length];
+  }
 );
 
-const coreSearch = (query: string, list: string[]) =>
-  list.map(v => ({ v, d: distance(query, v) }));
+const coreSearch = (query: string, list: string[], vague?: boolean) =>
+  list.map(v => ({ v, d: distance(query, v, vague) }));
 
 export const search = curry((query: string, list: string[]) => {
-  if (!query) {
-    return list;
-  }
-  return sortBy(prop("d"), coreSearch(query, list))
-    .filter(({ d }) => d < 1)
-    .map(prop("v"));
-});
-
-export const searchPreservingOrder = curry((query: string, list: string[]) => {
-  if (!query) {
-    return list;
-  }
-  return coreSearch(query, list)
-    .filter(({ d }) => d < 1)
-    .map(prop("v"));
-});
-
-export const vagueSearch = curry((query: string, list: string[]) => {
   if (!query) {
     return list;
   }
@@ -114,25 +91,48 @@ export const vagueSearch = curry((query: string, list: string[]) => {
     .map(prop("v"));
 });
 
+export const searchPreservingOrder = curry((query: string, list: string[]) => {
+  if (!query) {
+    return list;
+  }
+  return coreSearch(query, list)
+    .filter(({ d }) => d < 2)
+    .map(prop("v"));
+});
+
+export const vagueSearch = curry((query: string, list: string[]) => {
+  if (!query) {
+    return list;
+  }
+  return sortBy(prop("d"), coreSearch(query, list, true))
+    .filter(({ d }) => d < 3)
+    .map(prop("v"));
+});
+
 export const vagueSearchPreservingOrder = curry(
   (query: string, list: string[]) => {
     if (!query) {
       return list;
     }
-    return coreSearch(query, list)
-      .filter(({ d }) => d < 2)
+    return coreSearch(query, list, true)
+      .filter(({ d }) => d < 3)
       .map(prop("v"));
   }
 );
 
-const coreObjectSearch = <T>(query: string, keys: string[], list: T[]) =>
+const coreObjectSearch = <T>(
+  query: string,
+  keys: string[],
+  list: T[],
+  vague?: boolean
+) =>
   list.map(item => ({
     v: item,
     d: xprod(
       query.split(" "),
       keys.map(key => path(key.split("."), item))
     )
-      .map(([q, v]) => distance(q, `${v}`))
+      .map(([q, v]) => distance(q, `${v}`, vague))
       .reduce(min, Infinity)
   }));
 
@@ -142,7 +142,7 @@ export const objectSearch = curry(
       return list;
     }
     return sortBy(prop("d"), coreObjectSearch(query, keys, list))
-      .filter(({ d }) => d < 1)
+      .filter(({ d }) => d < 2)
       .map(prop("v"));
   }
 );
@@ -153,7 +153,7 @@ export const objectSearchPreservingOrder = curry(
       return list;
     }
     return coreObjectSearch(query, keys, list)
-      .filter(({ d }) => d < 1)
+      .filter(({ d }) => d < 2)
       .map(prop("v"));
   }
 );
@@ -163,8 +163,8 @@ export const vagueObjectSearch = curry(
     if (!query) {
       return list;
     }
-    return sortBy(prop("d"), coreObjectSearch(query, keys, list))
-      .filter(({ d }) => d < 2)
+    return sortBy(prop("d"), coreObjectSearch(query, keys, list, true))
+      .filter(({ d }) => d < 3)
       .map(prop("v"));
   }
 );
@@ -174,8 +174,8 @@ export const vagueObjectSearchPreservingOrder = curry(
     if (!query) {
       return list;
     }
-    return coreObjectSearch(query, keys, list)
-      .filter(({ d }) => d < 2)
+    return coreObjectSearch(query, keys, list, true)
+      .filter(({ d }) => d < 3)
       .map(prop("v"));
   }
 );
