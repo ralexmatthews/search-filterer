11bfefe809497f1ab246133258b865997f147c52
Alex Matthews <amatthews@dmp.com>
2020-02-27 09:22:03 -0600

    Removes memoization

--- dist/index.js
+++ dist/index.js
@@ -1,200 +1,182 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const ramda_1 = require("ramda");
-const distance = (() => {
-    const cache = {};
-    return (string1, string2) => {
-        const cacheKey = string1.length > string2.length
-            ? `string1:${string1} --- string2:${string2}`
-            : `string1:${string2} --- string2:${string1}`;
-        if (cacheKey in cache) {
-            return cache[cacheKey];
+const distance = (string1, string2) => {
+    const a = string1.trim().toLowerCase();
+    const b = string2.trim().toLowerCase();
+    if (!a.length || !b.length) {
+        return Infinity;
+    }
+    if (a === b) {
+        return -2;
+    }
+    if (a.length > b.length) {
+        if (a.substr(0, b.length) === b) {
+            return -1;
         }
-        if (!string1.length || !string2.length) {
+        if (!!a.match(b)) {
             return 0;
         }
-        const a = string1.trim().toLowerCase();
-        const b = string2.trim().toLowerCase();
-        if (a === b) {
-            return -2;
+    }
+    else if (b.length > a.length) {
+        if (b.substr(0, a.length) === a) {
+            return -1;
         }
-        if (a.length > b.length) {
-            if (a.substr(0, b.length) === b) {
-                return -1;
-            }
-            if (!!a.match(b)) {
-                return 0;
-            }
+        if (!!b.match(a)) {
+            return 0;
         }
-        else if (b.length > a.length) {
-            if (b.substr(0, a.length) === a) {
-                return -1;
-            }
-            if (!!b.match(a)) {
-                return 0;
-            }
-        }
-        if (a.split(" ").length > 1 && b.split(" ").length > 1) {
-            let subQueries = b.split(" ");
-            let score = Infinity;
-            a.split(" ").forEach(subTerm => {
-                const subTermScores = subQueries.map(subQuery => ({
-                    v: subQuery,
-                    d: distance(subQuery, subTerm)
-                }));
-                const minValue = subTermScores.reduce((acc, obj) => (acc.d < obj.d ? acc : obj), subTermScores[0]);
-                score = ramda_1.mean([minValue.d].concat(score !== Infinity ? score : minValue.d));
-                subQueries = subQueries.filter(item => item !== minValue.v);
-            });
-            return score;
-        }
-        const d = [];
-        for (let i = 0; i <= a.length; i++) {
-            d.push([i]);
-        }
-        for (let j = 0; j <= b.length; j++) {
-            d[0][j] = j;
-        }
-        for (let i = 1; i <= a.length; i++) {
-            for (let j = 1; j <= b.length; j++) {
-                const cost = a[i - 1] === b[j - 1] ? 0 : 1;
-                d[i][j] = ramda_1.min(ramda_1.min(d[i - 1][j] + 1, d[i][j - 1] + 1), d[i - 1][j - 1] + cost);
-                if (i > 1 && j > 1 && a[i - 1] === b[j - 2] && a[i - 2] === b[j - 1]) {
-                    d[i][j] = ramda_1.min(d[i][j], d[i - 2][j - 2] + 1);
-                }
+    }
+    const aWords = a.split(" ");
+    const bWords = b.split(" ");
+    if (aWords.length > 1 || bWords.length > 1) {
+        let subQueries = aWords.length >= bWords.length ? aWords : bWords;
+        let score = Infinity;
+        (aWords.length < bWords.length ? aWords : bWords).forEach(subTerm => {
+            const subTermScores = subQueries.map(subQuery => ({
+                v: subQuery,
+                d: distance(subQuery, subTerm)
+            }));
+            const minValue = subTermScores.reduce((acc, obj) => (acc.d < obj.d ? acc : obj), subTermScores[0]);
+            score = (minValue.d + (score !== Infinity ? score : minValue.d)) / 2;
+            subQueries.splice(subQueries.indexOf(minValue.v), 1);
+        });
+        return score;
+    }
+    const d = [];
+    for (let i = 0; i <= a.length; i++) {
+        d.push([i]);
+    }
+    for (let j = 0; j <= b.length; j++) {
+        d[0][j] = j;
+    }
+    for (let i = 1; i <= a.length; i++) {
+        for (let j = 1; j <= b.length; j++) {
+            const cost = a[i - 1] === b[j - 1] ? 0 : 1;
+            d[i][j] = ramda_1.min(ramda_1.min(d[i - 1][j] + 1, d[i][j - 1] + 1), d[i - 1][j - 1] + cost);
+            if (i > 1 && j > 1 && a[i - 1] === b[j - 2] && a[i - 2] === b[j - 1]) {
+                d[i][j] = ramda_1.min(d[i][j], d[i - 2][j - 2] + 1);
             }
         }
-        cache[cacheKey] = d[a.length][b.length];
-        return d[a.length][b.length];
-    };
-})();
+    }
+    return d[a.length][b.length];
+};
 const coreSearch = (query, list) => list.map(item => ({
     v: item,
     d: distance(query, item)
 }));
-exports.search = ramda_1.curry(ramda_1.memoizeWith((query, list) => `${query} --- ${JSON.stringify(list)}`, (query, list) => {
+exports.search = ramda_1.curry((query, list) => {
     if (!query) {
         return list;
     }
     return ramda_1.sortBy(ramda_1.prop("d"), coreSearch(query, list))
         .filter(({ d }) => d < 2)
         .map(ramda_1.prop("v"));
-}));
-exports.searchPreservingOrder = ramda_1.curry(ramda_1.memoizeWith((query, list) => `${query} --- ${JSON.stringify(list)}`, (query, list) => {
+});
+exports.searchPreservingOrder = ramda_1.curry((query, list) => {
     if (!query) {
         return list;
     }
     return coreSearch(query, list)
         .filter(({ d }) => d < 2)
         .map(ramda_1.prop("v"));
-}));
-exports.vagueSearch = ramda_1.curry(ramda_1.memoizeWith((query, list) => `${query} --- ${JSON.stringify(list)}`, (query, list) => {
+});
+exports.vagueSearch = ramda_1.curry((query, list) => {
     if (!query) {
         return list;
     }
     return ramda_1.sortBy(ramda_1.prop("d"), coreSearch(query, list))
         .filter(({ d }) => d < 3)
         .map(ramda_1.prop("v"));
-}));
-exports.vagueSearchPreservingOrder = ramda_1.curry(ramda_1.memoizeWith((query, list) => `${query} --- ${JSON.stringify(list)}`, (query, list) => {
+});
+exports.vagueSearchPreservingOrder = ramda_1.curry((query, list) => {
     if (!query) {
         return list;
     }
     return coreSearch(query, list)
         .filter(({ d }) => d < 3)
         .map(ramda_1.prop("v"));
-}));
+});
 const coreObjectSearch = (query, keys, list) => list.map(item => {
     const scores = keys
         .map(key => ramda_1.path(key.split("."), item))
-        .filter(ramda_1.complement(ramda_1.isNil))
-        .map(term => {
-        if (typeof term !== "string") {
-            return 0;
-        }
-        return distance(term, query);
-    });
+        .filter(v => typeof v === "string")
+        .map(term => distance(`${term}`, query));
     return {
         v: item,
         d: scores.reduce(ramda_1.min, Infinity)
     };
 });
-exports.objectSearch = ramda_1.curry(ramda_1.memoizeWith((query, keys, list) => `${query} --- ${JSON.stringify(keys)} --- ${JSON.stringify(list)}`, (query, keys, list) => {
+exports.objectSearch = ramda_1.curry((query, keys, list) => {
     if (!query) {
         return list;
     }
     return ramda_1.sortBy(ramda_1.prop("d"), coreObjectSearch(query, keys, list))
         .filter(({ d }) => d < 2)
         .map(ramda_1.prop("v"));
-}));
-exports.objectSearchPreservingOrder = ramda_1.curry(ramda_1.memoizeWith((query, keys, list) => `${query} --- ${JSON.stringify(keys)} --- ${JSON.stringify(list)}`, (query, keys, list) => {
+});
+exports.objectSearchPreservingOrder = ramda_1.curry((query, keys, list) => {
     if (!query) {
         return list;
     }
     return coreObjectSearch(query, keys, list)
         .filter(({ d }) => d < 2)
         .map(ramda_1.prop("v"));
-}));
-exports.vagueObjectSearch = ramda_1.curry(ramda_1.memoizeWith((query, keys, list) => `${query} --- ${JSON.stringify(keys)} --- ${JSON.stringify(list)}`, (query, keys, list) => {
+});
+exports.vagueObjectSearch = ramda_1.curry((query, keys, list) => {
     if (!query) {
         return list;
     }
     return ramda_1.sortBy(ramda_1.prop("d"), coreObjectSearch(query, keys, list))
         .filter(({ d }) => d < 3)
         .map(ramda_1.prop("v"));
-}));
-exports.vagueObjectSearchPreservingOrder = ramda_1.curry(ramda_1.memoizeWith((query, keys, list) => `${query} --- ${JSON.stringify(keys)} --- ${JSON.stringify(list)}`, (query, keys, list) => {
+});
+exports.vagueObjectSearchPreservingOrder = ramda_1.curry((query, keys, list) => {
     if (!query) {
         return list;
     }
     return coreObjectSearch(query, keys, list)
         .filter(({ d }) => d < 3)
         .map(ramda_1.prop("v"));
-}));
+});
 const coreGetterSearch = (query, getters, list) => list.map(item => {
     const scores = getters
         .map(getter => getter(item))
-        .filter(ramda_1.complement(ramda_1.isNil))
-        .map(term => {
-        if (typeof term !== "string") {
-            return 0;
-        }
-        return distance(term, query);
-    });
+        .filter(v => typeof v === "string")
+        .map(term => distance(term, query));
     return {
         v: item,
         d: scores.reduce(ramda_1.min, Infinity)
     };
 });
-exports.searchUsingGetters = ramda_1.curry(ramda_1.memoizeWith((query, getters, list) => `${query} --- ${JSON.stringify(getters.map(v => v.name || Math.random()))} --- ${JSON.stringify(list)}`, (query, getters, list) => {
+exports.searchUsingGetters = ramda_1.curry((query, getters, list) => {
     if (!query) {
         return list;
     }
     return ramda_1.sortBy(ramda_1.prop("d"), coreGetterSearch(query, getters, list))
         .filter(({ d }) => d < 2)
         .map(ramda_1.prop("v"));
-}));
-exports.searchUsingGettersPreservingOrder = ramda_1.curry(ramda_1.memoizeWith((query, getters, list) => `${query} --- ${JSON.stringify(getters.map(v => v.name || Math.random()))} --- ${JSON.stringify(list)}`, (query, getters, list) => {
+});
+exports.searchUsingGettersPreservingOrder = ramda_1.curry((query, getters, list) => {
     if (!query) {
         return list;
     }
     return coreGetterSearch(query, getters, list)
         .filter(({ d }) => d < 2)
         .map(ramda_1.prop("v"));
-}));
-exports.vagueSearchUsingGetters = ramda_1.curry(ramda_1.memoizeWith((query, getters, list) => `${query} --- ${JSON.stringify(getters.map(v => v.name || Math.random()))} --- ${JSON.stringify(list)}`, (query, getters, list) => {
+});
+exports.vagueSearchUsingGetters = ramda_1.curry((query, getters, list) => {
     if (!query) {
         return list;
     }
     return ramda_1.sortBy(ramda_1.prop("d"), coreGetterSearch(query, getters, list))
         .filter(({ d }) => d < 3)
         .map(ramda_1.prop("v"));
-}));
-exports.vagueSearchUsingGettersPreservingOrder = ramda_1.curry(ramda_1.memoizeWith((query, getters, list) => `${query} --- ${JSON.stringify(getters.map(v => v.name || Math.random()))} --- ${JSON.stringify(list)}`, (query, getters, list) => {
+});
+exports.vagueSearchUsingGettersPreservingOrder = ramda_1.curry((query, getters, list) => {
     if (!query) {
         return list;
     }
     return coreGetterSearch(query, getters, list)
         .filter(({ d }) => d < 3)
         .map(ramda_1.prop("v"));
-}));
+});
--- src/index.ts
+++ src/index.ts
@@ -1,103 +1,81 @@
-import {
-  curry,
-  mean,
-  min,
-  path,
-  prop,
-  sortBy,
-  zip,
-  complement,
-  isNil,
-  memoizeWith
-} from "ramda";
+import { curry, min, path, prop, sortBy } from "ramda";
 
-const distance: (a: string, b: string) => number = (() => {
-  const cache: { [key: string]: number } = {};
+const distance: (a: string, b: string) => number = (
+  string1: string,
+  string2: string
+) => {
+  const a = string1.trim().toLowerCase();
+  const b = string2.trim().toLowerCase();
 
-  return (string1: string, string2: string) => {
-    const cacheKey =
-      string1.length > string2.length
-        ? `string1:${string1} --- string2:${string2}`
-        : `string1:${string2} --- string2:${string1}`;
+  if (!a.length || !b.length) {
+    return Infinity;
+  }
+  if (a === b) {
+    return -2;
+  }
 
-    if (cacheKey in cache) {
-      return cache[cacheKey];
+  if (a.length > b.length) {
+    if (a.substr(0, b.length) === b) {
+      return -1;
     }
-    if (!string1.length || !string2.length) {
+    if (!!a.match(b)) {
       return 0;
     }
-    const a = string1.trim().toLowerCase();
-    const b = string2.trim().toLowerCase();
-
-    if (a === b) {
-      return -2;
+  } else if (b.length > a.length) {
+    if (b.substr(0, a.length) === a) {
+      return -1;
     }
-
-    if (a.length > b.length) {
-      if (a.substr(0, b.length) === b) {
-        return -1;
-      }
-      if (!!a.match(b)) {
-        return 0;
-      }
-    } else if (b.length > a.length) {
-      if (b.substr(0, a.length) === a) {
-        return -1;
-      }
-      if (!!b.match(a)) {
-        return 0;
-      }
+    if (!!b.match(a)) {
+      return 0;
     }
+  }
 
-    if (a.split(" ").length > 1 && b.split(" ").length > 1) {
-      let subQueries = b.split(" ");
-      let score = Infinity;
-      a.split(" ").forEach(subTerm => {
-        const subTermScores = subQueries.map(subQuery => ({
-          v: subQuery,
-          d: distance(subQuery, subTerm)
-        }));
-        const minValue = subTermScores.reduce(
-          (acc, obj) => (acc.d < obj.d ? acc : obj),
-          subTermScores[0]
-        );
-        score = mean(
-          [minValue.d].concat(score !== Infinity ? score : minValue.d)
-        );
-        subQueries = subQueries.filter(item => item !== minValue.v);
-      });
-      return score;
-    }
+  const aWords = a.split(" ");
+  const bWords = b.split(" ");
+  if (aWords.length > 1 || bWords.length > 1) {
+    let subQueries = aWords.length >= bWords.length ? aWords : bWords;
+    let score = Infinity;
+    (aWords.length < bWords.length ? aWords : bWords).forEach(subTerm => {
+      const subTermScores = subQueries.map(subQuery => ({
+        v: subQuery,
+        d: distance(subQuery, subTerm)
+      }));
+      const minValue = subTermScores.reduce(
+        (acc, obj) => (acc.d < obj.d ? acc : obj),
+        subTermScores[0]
+      );
+      score = (minValue.d + (score !== Infinity ? score : minValue.d)) / 2;
+      subQueries.splice(subQueries.indexOf(minValue.v), 1);
+    });
+    return score;
+  }
 
-    const d = [];
+  const d = [];
 
-    for (let i = 0; i <= a.length; i++) {
-      d.push([i]);
-    }
+  for (let i = 0; i <= a.length; i++) {
+    d.push([i]);
+  }
 
-    for (let j = 0; j <= b.length; j++) {
-      d[0][j] = j;
-    }
+  for (let j = 0; j <= b.length; j++) {
+    d[0][j] = j;
+  }
 
-    for (let i = 1; i <= a.length; i++) {
-      for (let j = 1; j <= b.length; j++) {
-        const cost = a[i - 1] === b[j - 1] ? 0 : 1;
+  for (let i = 1; i <= a.length; i++) {
+    for (let j = 1; j <= b.length; j++) {
+      const cost = a[i - 1] === b[j - 1] ? 0 : 1;
 
-        d[i][j] = min(
-          min(d[i - 1][j] + 1, d[i][j - 1] + 1),
-          d[i - 1][j - 1] + cost
-        );
+      d[i][j] = min(
+        min(d[i - 1][j] + 1, d[i][j - 1] + 1),
+        d[i - 1][j - 1] + cost
+      );
 
-        if (i > 1 && j > 1 && a[i - 1] === b[j - 2] && a[i - 2] === b[j - 1]) {
-          d[i][j] = min(d[i][j], d[i - 2][j - 2] + 1);
-        }
+      if (i > 1 && j > 1 && a[i - 1] === b[j - 2] && a[i - 2] === b[j - 1]) {
+        d[i][j] = min(d[i][j], d[i - 2][j - 2] + 1);
       }
     }
-
-    cache[cacheKey] = d[a.length][b.length];
-    return d[a.length][b.length];
-  };
-})();
+  }
+  return d[a.length][b.length];
+};
 
 const coreSearch = (query: string, list: string[]) =>
   list.map(item => ({
@@ -105,74 +83,50 @@ const coreSearch = (query: string, list: string[]) =>
     d: distance(query, item)
   }));
 
-export const search = curry(
-  memoizeWith(
-    (query: string, list: string[]) => `${query} --- ${JSON.stringify(list)}`,
-    (query: string, list: string[]) => {
-      if (!query) {
-        return list;
-      }
-      return sortBy(prop("d"), coreSearch(query, list))
-        .filter(({ d }) => d < 2)
-        .map(prop("v"));
-    }
-  )
-);
+export const search = curry((query: string, list: string[]) => {
+  if (!query) {
+    return list;
+  }
+  return sortBy(prop("d"), coreSearch(query, list))
+    .filter(({ d }) => d < 2)
+    .map(prop("v"));
+});
 
-export const searchPreservingOrder = curry(
-  memoizeWith(
-    (query: string, list: string[]) => `${query} --- ${JSON.stringify(list)}`,
-    (query: string, list: string[]) => {
-      if (!query) {
-        return list;
-      }
-      return coreSearch(query, list)
-        .filter(({ d }) => d < 2)
-        .map(prop("v"));
-    }
-  )
-);
+export const searchPreservingOrder = curry((query: string, list: string[]) => {
+  if (!query) {
+    return list;
+  }
+  return coreSearch(query, list)
+    .filter(({ d }) => d < 2)
+    .map(prop("v"));
+});
 
-export const vagueSearch = curry(
-  memoizeWith(
-    (query: string, list: string[]) => `${query} --- ${JSON.stringify(list)}`,
-    (query: string, list: string[]) => {
-      if (!query) {
-        return list;
-      }
-      return sortBy(prop("d"), coreSearch(query, list))
-        .filter(({ d }) => d < 3)
-        .map(prop("v"));
-    }
-  )
-);
+export const vagueSearch = curry((query: string, list: string[]) => {
+  if (!query) {
+    return list;
+  }
+  return sortBy(prop("d"), coreSearch(query, list))
+    .filter(({ d }) => d < 3)
+    .map(prop("v"));
+});
 
 export const vagueSearchPreservingOrder = curry(
-  memoizeWith(
-    (query: string, list: string[]) => `${query} --- ${JSON.stringify(list)}`,
-    (query: string, list: string[]) => {
-      if (!query) {
-        return list;
-      }
-      return coreSearch(query, list)
-        .filter(({ d }) => d < 3)
-        .map(prop("v"));
+  (query: string, list: string[]) => {
+    if (!query) {
+      return list;
     }
-  )
+    return coreSearch(query, list)
+      .filter(({ d }) => d < 3)
+      .map(prop("v"));
+  }
 );
 
 const coreObjectSearch = <T>(query: string, keys: string[], list: T[]) =>
   list.map(item => {
     const scores = keys
       .map(key => path(key.split("."), item))
-      .filter(complement(isNil))
-      .map(term => {
-        if (typeof term !== "string") {
-          return 0;
-        }
-
-        return distance(term, query);
-      });
+      .filter(v => typeof v === "string")
+      .map(term => distance(`${term}`, query));
     return {
       v: item,
       d: scores.reduce(min, Infinity)
@@ -180,77 +134,55 @@ const coreObjectSearch = <T>(query: string, keys: string[], list: T[]) =>
   });
 
 export const objectSearch = curry(
-  memoizeWith(
-    <T>(query: string, keys: string[], list: T[]) =>
-      `${query} --- ${JSON.stringify(keys)} --- ${JSON.stringify(list)}`,
-    <T>(query: string, keys: string[], list: T[]) => {
-      if (!query) {
-        return list;
-      }
-      return sortBy(prop("d"), coreObjectSearch(query, keys, list))
-        .filter(({ d }) => d < 2)
-        .map(prop("v"));
+  <T>(query: string, keys: string[], list: T[]) => {
+    if (!query) {
+      return list;
     }
-  )
+    return sortBy(prop("d"), coreObjectSearch(query, keys, list))
+      .filter(({ d }) => d < 2)
+      .map(prop("v"));
+  }
 );
 
 export const objectSearchPreservingOrder = curry(
-  memoizeWith(
-    <T>(query: string, keys: string[], list: T[]) =>
-      `${query} --- ${JSON.stringify(keys)} --- ${JSON.stringify(list)}`,
-    <T>(query: string, keys: string[], list: T[]) => {
-      if (!query) {
-        return list;
-      }
-      return coreObjectSearch(query, keys, list)
-        .filter(({ d }) => d < 2)
-        .map(prop("v"));
+  <T>(query: string, keys: string[], list: T[]) => {
+    if (!query) {
+      return list;
     }
-  )
+    return coreObjectSearch(query, keys, list)
+      .filter(({ d }) => d < 2)
+      .map(prop("v"));
+  }
 );
 
 export const vagueObjectSearch = curry(
-  memoizeWith(
-    <T>(query: string, keys: string[], list: T[]) =>
-      `${query} --- ${JSON.stringify(keys)} --- ${JSON.stringify(list)}`,
-    <T>(query: string, keys: string[], list: T[]) => {
-      if (!query) {
-        return list;
-      }
-      return sortBy(prop("d"), coreObjectSearch(query, keys, list))
-        .filter(({ d }) => d < 3)
-        .map(prop("v"));
+  <T>(query: string, keys: string[], list: T[]) => {
+    if (!query) {
+      return list;
     }
-  )
+    return sortBy(prop("d"), coreObjectSearch(query, keys, list))
+      .filter(({ d }) => d < 3)
+      .map(prop("v"));
+  }
 );
 
 export const vagueObjectSearchPreservingOrder = curry(
-  memoizeWith(
-    <T>(query: string, keys: string[], list: T[]) =>
-      `${query} --- ${JSON.stringify(keys)} --- ${JSON.stringify(list)}`,
-    <T>(query: string, keys: string[], list: T[]) => {
-      if (!query) {
-        return list;
-      }
-      return coreObjectSearch(query, keys, list)
-        .filter(({ d }) => d < 3)
-        .map(prop("v"));
+  <T>(query: string, keys: string[], list: T[]) => {
+    if (!query) {
+      return list;
     }
-  )
+    return coreObjectSearch(query, keys, list)
+      .filter(({ d }) => d < 3)
+      .map(prop("v"));
+  }
 );
 
 const coreGetterSearch = <T>(query: string, getters: Function[], list: T[]) =>
   list.map(item => {
     const scores = getters
       .map(getter => getter(item))
-      .filter(complement(isNil))
-      .map(term => {
-        if (typeof term !== "string") {
-          return 0;
-        }
-
-        return distance(term, query);
-      });
+      .filter(v => typeof v === "string")
+      .map(term => distance(term, query));
     return {
       v: item,
       d: scores.reduce(min, Infinity)
@@ -258,69 +190,45 @@ const coreGetterSearch = <T>(query: string, getters: Function[], list: T[]) =>
   });
 
 export const searchUsingGetters = curry(
-  memoizeWith(
-    <T>(query: string, getters: Function[], list: T[]) =>
-      `${query} --- ${JSON.stringify(
-        getters.map(v => v.name || Math.random())
-      )} --- ${JSON.stringify(list)}`,
-    <T>(query: string, getters: Function[], list: T[]) => {
-      if (!query) {
-        return list;
-      }
-      return sortBy(prop("d"), coreGetterSearch(query, getters, list))
-        .filter(({ d }) => d < 2)
-        .map(prop("v"));
+  <T>(query: string, getters: Function[], list: T[]) => {
+    if (!query) {
+      return list;
     }
-  )
+    return sortBy(prop("d"), coreGetterSearch(query, getters, list))
+      .filter(({ d }) => d < 2)
+      .map(prop("v"));
+  }
 );
 
 export const searchUsingGettersPreservingOrder = curry(
-  memoizeWith(
-    <T>(query: string, getters: Function[], list: T[]) =>
-      `${query} --- ${JSON.stringify(
-        getters.map(v => v.name || Math.random())
-      )} --- ${JSON.stringify(list)}`,
-    <T>(query: string, getters: Function[], list: T[]) => {
-      if (!query) {
-        return list;
-      }
-      return coreGetterSearch(query, getters, list)
-        .filter(({ d }) => d < 2)
-        .map(prop("v"));
+  <T>(query: string, getters: Function[], list: T[]) => {
+    if (!query) {
+      return list;
     }
-  )
+    return coreGetterSearch(query, getters, list)
+      .filter(({ d }) => d < 2)
+      .map(prop("v"));
+  }
 );
 
 export const vagueSearchUsingGetters = curry(
-  memoizeWith(
-    <T>(query: string, getters: Function[], list: T[]) =>
-      `${query} --- ${JSON.stringify(
-        getters.map(v => v.name || Math.random())
-      )} --- ${JSON.stringify(list)}`,
-    <T>(query: string, getters: Function[], list: T[]) => {
-      if (!query) {
-        return list;
-      }
-      return sortBy(prop("d"), coreGetterSearch(query, getters, list))
-        .filter(({ d }) => d < 3)
-        .map(prop("v"));
+  <T>(query: string, getters: Function[], list: T[]) => {
+    if (!query) {
+      return list;
     }
-  )
+    return sortBy(prop("d"), coreGetterSearch(query, getters, list))
+      .filter(({ d }) => d < 3)
+      .map(prop("v"));
+  }
 );
 
 export const vagueSearchUsingGettersPreservingOrder = curry(
-  memoizeWith(
-    <T>(query: string, getters: Function[], list: T[]) =>
-      `${query} --- ${JSON.stringify(
-        getters.map(v => v.name || Math.random())
-      )} --- ${JSON.stringify(list)}`,
-    <T>(query: string, getters: Function[], list: T[]) => {
-      if (!query) {
-        return list;
-      }
-      return coreGetterSearch(query, getters, list)
-        .filter(({ d }) => d < 3)
-        .map(prop("v"));
+  <T>(query: string, getters: Function[], list: T[]) => {
+    if (!query) {
+      return list;
     }
-  )
+    return coreGetterSearch(query, getters, list)
+      .filter(({ d }) => d < 3)
+      .map(prop("v"));
+  }
 );
